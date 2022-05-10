8a3a904b21e73b805887dab94cf0a9a04fe84e70
luv-jeri <luvvjeri@gmail.com>
2022-05-10 21:21:16 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+// const useState = (initial) => {
+//   let specialVariable = initial;
+//   const aFunctionThatChangesTheSpecialVariable = (value) => {
+//     specialVariable = value;
+//     root.render();
+//   };
+//   return [specialVariable, aFunctionThatChangesTheSpecialVariable];
+// };
 
 function App() {
+  const classForTheContainer = 'container';
+
+  const special = useState(0);
+  const [count, setCount] = special;
+
+  const [color, setColor] = useState('black');
+
+  const boxStyle = {
+    margin: '10px',
+    border: '1px solid red',
+    borderRadius: '10%',
+    backgroundColor: color,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={classForTheContainer}>
+      <button
+        className='btn'
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        className='btn'
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+      <div className='wrap'>
+        <div className='box' style={boxStyle}></div>
+      </div>
+      {count}
+      <div className='wrap'>
+        <button
+          className='btn'
+          onClick={() => {
+            setColor('red');
+            console.log(color);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Red
+        </button>
+        <button
+          className='btn'
+          onClick={() => {
+            setColor('Yellow');
+            console.log(color);
+          }}
+        >
+          Yellow
+        </button>
+        <button
+          className='btn'
+          onClick={() => {
+            setColor('Green');
+            console.log(color);
+          }}
+        >
+          Green
+        </button>
+        <button
+          className='btn'
+          onClick={() => {
+            setColor('Blue');
+            console.log(color);
+          }}
+        >
+          Blue
+        </button>
+      </div>
     </div>
   );
 }
