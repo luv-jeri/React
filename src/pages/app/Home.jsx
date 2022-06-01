b848a6ce3f8c15a77f1f97bded48c4bec8ccb6dc
luv-jeri <luvvjeri@gmail.com>
2022-06-01 21:41:01 +0530

    -

--- src/pages/app/Home.jsx
+++ src/pages/app/Home.jsx
@@ -1,46 +1,97 @@
-import React from 'react';
-import { AppShell, Navbar, Header, Button } from '@mantine/core';
+import React, { useState } from 'react';
+import {
+  Menu,
+  AppShell,
+  Navbar,
+  Header,
+  Button,
+  Footer,
+  Avatar,
+  Text,
+  Divider,
+} from '@mantine/core';
 import { At, PhoneCall, MoodSmile } from 'tabler-icons-react';
+import {
+  Settings,
+  Search,
+  Photo,
+  MessageCircle,
+  Trash,
+  ArrowsLeftRight,
+  FilePlus,
+} from 'tabler-icons-react';
 import useAuth from '../../context/Auth.context';
+import greeting from '../../utils/greeting';
+
 export default function Home() {
   const { logout, user } = useAuth();
 
-  console.log(user);
   return (
     <AppShell
       padding='md'
       navbar={
         <Navbar width={{ base: 300 }} height={500} p='xs'>
-          <Navbar.Section>{/* Header with logo */}</Navbar.Section>
-          <Navbar.Section grow mt='md'></Navbar.Section>
-          <Navbar.Section>{/* Footer with user */}</Navbar.Section>
+          <Navbar.Section>
+            <FilePlus size={24} />
+          </Navbar.Section>
         </Navbar>
       }
+      footer={
+        <Footer height={60} p='md'>
+          Application footer
+        </Footer>
+      }
       header={
-        <Header height={60} p='xs'>
-          <Button
-            leftIcon={<MoodSmile size={24} />}
-            fullWidth
-            variant='subtle'
-            color='grape'
-            radius='lg'
-            size='md'
-            onClick={() => {
-              logout();
+        <Header
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+          height={60}
+          p='xs'
+        >
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
             }}
           >
-            Logout
-          </Button>
+            <Avatar radius='xl' src={user.DP} alt="it's me" />
+            <Text
+              style={{
+                marginLeft: '1rem',
+              }}
+              weight={700}
+            >
+              {greeting()} {user.firstName}
+            </Text>
+          </div>
+          <Menu
+            trigger='hover'
+            transition='slide-up'
+            transitionDuration={100}
+            transitionTimingFunction='ease'
+          >
+            <Menu.Label>Application</Menu.Label>
+            <Menu.Item icon={<Settings size={14} />}>OK</Menu.Item>
+            <Menu.Item icon={<Photo size={14} />}>Profile</Menu.Item>
+            <Divider />
+            <Menu.Label>Danger zone</Menu.Label>
+            <Menu.Item
+              color='red'
+              icon={<Trash size={14} />}
+              onClick={() => {
+                logout();
+              }}
+            >
+              Logout
+            </Menu.Item>
+          </Menu>
         </Header>
       }
-      styles={(theme) => ({
-        main: {
-          backgroundColor:
-            theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-        },
-      })}
     >
-      {/* Your application here */}
+      App here
     </AppShell>
   );
 }
