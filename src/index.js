e6bd4bf28093da4e54145826b8fe0228d69cee78
luv-jeri <luvvjeri@gmail.com>
2022-06-27 21:00:18 +0530

    -

--- src/index.js
+++ src/index.js
@@ -6,19 +6,27 @@ import { MantineProvider } from '@mantine/core';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/Auth.context';
 import { NotificationsProvider } from '@mantine/notifications';
+import { createStore } from './redux';
+import { Provider } from 'react-redux';
+
+const store = createStore((state = 0, action) => {
+  return state;
+});
 
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
-    <NotificationsProvider position='top-right' limit={10}>
-      <Router>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </Router>
-    </NotificationsProvider>
-  </MantineProvider>
+  <Provider store={store}>
+    <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
+      <NotificationsProvider position='top-right' limit={10}>
+        <Router>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </Router>
+      </NotificationsProvider>
+    </MantineProvider>
+  </Provider>
 );
